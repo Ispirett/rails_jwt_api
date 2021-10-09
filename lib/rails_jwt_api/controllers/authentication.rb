476e0aa4583d707edc4ea4ce9bdd1595e8555137
ispirett <ispirett@gmail.com>
2021-10-09 00:54:58 -0400

    refactor(module): authentication

--- lib/rails_jwt_api/controllers/authentication.rb
+++ lib/rails_jwt_api/controllers/authentication.rb
@@ -2,16 +2,16 @@ module RailsJwtApi
   module Controllers
     module Authentication
 
-      SECRET_KEY =  ENV.fetch("RAILS_JWT_TOKEN",RailsJwtApi.token_secret_key)
+
       # TODO refactor to allow users to ad there own expiration date
       def encode(payout, exp = RailsJwtApi.token_expiration)
         payout[:exp] = exp.to_i
-        JWT.encode(payout, SECRET_KEY)
+        JWT.encode(payout, RailsJwtApi.token_secret_key)
     
       end
     
       def decode(token)
-        decode = JWT.decode(token, SECRET_KEY)[0]
+        decode = JWT.decode(token,RailsJwtApi.token_secret_key)[0]
         HashWithIndifferentAccess.new decode
       end
     end 
--- lib/rails_jwt_api/version.rb
+++ lib/rails_jwt_api/version.rb
@@ -1,3 +1,3 @@
 module RailsJwtApi
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
