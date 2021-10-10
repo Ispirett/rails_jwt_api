32f405b3da3c6942ae4c150c735256d0ad9084f4
ispirett <ispirett@gmail.com>
2021-10-10 10:12:36 -0400

    feature(verify_phone)

--- app/controllers/rails_jwt_api/users_controller.rb
+++ app/controllers/rails_jwt_api/users_controller.rb
@@ -13,10 +13,7 @@ module  RailsJwtApi
       if @user.save
 
         if RailsJwtApi.phone_verification
-          random_number = generate_random_number
-          @user.update(verification_number: random_number)
-          send_message(body: "Here is you verification code #{random_number}", to: @user.details.phone)
-          return render json: {status: 'success', user: @user.details, msg: "Please verify phone number"}, status: :ok
+          return initialize_verification(@user)
         end
 
         token = encode(user_id: @user.id)
@@ -24,14 +21,26 @@ module  RailsJwtApi
         time =  Time.now +  RailsJwtApi.token_expiration.to_i
         # TODO if phone verification enable then send response informing user message has been sent to their account
 
-        render json: {status: 'success', token: token,user: @user.details, exp: time.strftime('%m %d %y %H:%M')}, status: :ok
+        render json: {status: 'success', token: token,user: @user.details, exp: time.strftime('%m %d %y %H:%M')}, 
+status: :ok
       else
         render json: {status: :failed, msg: @user.errors.full_messages}, status: :unauthorized
       end
     end
 
     def verify_phone
-      User.find
+      user =  User.find_by(id: params[:user_id])
+      if user
+        if user.verification_number == params[:verification_number]
+          token = encode(user_id: @user.id)
+          time =  Time.now +  RailsJwtApi.token_expiration.to_i
+          render json: {status: 'success', token: token,user: @user.details, exp: time.strftime('%m %d %y %H:%M')}, status: :ok
+        else
+          initialize_verification(user)
+        end
+      else
+        render json: {status: 'success', msg: 'User does not exist', exp: time.strftime('%m %d %y %H:%M')}, status: :unauthorized
+      end
     end
 
     def sign_in
@@ -48,7 +57,8 @@ module  RailsJwtApi
           Jwt.create(user_id: @user.id, token: token)
         end
 
-        render json: {status: :success,user: @user.details, token:token, exp: time.strftime('%m-%d-%y %H:%M')},status: :ok
+        render json: {status: :success,user: @user.details, token:token, exp: time.strftime('%m-%d-%y %H:%M')},
+status: :ok
       else
         render json: {status: :failed, msg: 'email or password is incorrect'}, status: :unauthorized
       end
@@ -72,6 +82,13 @@ module  RailsJwtApi
         elsif user_params[:password] != user_params[:password_confirmation]
         render json: {status: :failed, msg: "Your passwords do not match." , status: :unauthorize}
       end
+    end
+
+    def initialize_verification(user)
+      random_number = generate_random_number
+      user.update(verification_number: random_number)
+      send_message(body: "Here is you verification code #{random_number}", to: user.details.phone)
+      render json: {status: 'success', user: user.details, msg: "Please verify phone number"}, status: :ok
     end
   end
 
